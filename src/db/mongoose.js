3f0d31c33a09e1f54d8c884c5f310d473edf8c45
ogbenisamu <samijiyemi@gmail.com>
2022-08-29 05:20:40 +0100

    add dotenv package

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
+const path = require("path");
 
-mongoose.connect("mongodb://127.0.0.1:27017/guestbook", (err, data) => {
-  console.log("connected to the database");
+const filePath = path.join(__dirname, "../", "/config", "dev.env");
+
+require("dotenv").config({ path: filePath });
+
+mongoose.connect(process.env.MONGODB_URI, (err, data) => {
+  if (err) {
+    return console.error("unable to connect the database");
+  }
+  console.log("database connected");
 });
 
 module.exports = mongoose;
